470082af4917ba976efa9c7da878552db846b08d
LorikPopaj <lp51861@ubt-uni.net>
2023-03-12 18:09:16 +0100

    create_edit

---
+++ src/components/CreateComponent.vue
@@ -0,0 +1,87 @@
+<template>
+    <div class="row justify-content-center">
+      <div class="col-md-6">
+        <h3 class="text-center">Create game</h3>
+        <form @submit.prevent="handleSubmitForm">
+          <div class="form-group">
+            <label>Name</label>
+            <input
+              type="text"
+              class="form-control"
+              v-model="game.name"
+              required
+            />
+          </div>
+  
+          <div class="form-group">
+            <label>Email</label>
+            <input
+              type="email"
+              class="form-control"
+              v-model="game.email"
+              required
+            />
+          </div>
+  
+          <div class="form-group">
+            <label>Phone</label>
+            <input
+              type="text"
+              class="form-control"
+              v-model="game.phone"
+              required
+            />
+          </div>
+          <div class="form-group">
+            <label>Images</label>
+            <input
+              type="img"
+              class="form-control"
+              v-model="game.image"
+              required
+            />
+          </div>
+  
+          <div class="form-group">
+            <button class="btn btn-danger btn-block">Create</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import axios from "axios";
+  
+  export default {
+    data() {
+      return {
+        game: {
+          name: "",
+          email: "",
+          phone: "",
+        },
+      };
+    },
+    methods: {
+      handleSubmitForm() {
+        let apiURL = "http://localhost:4000/api/create-game";
+  
+        axios
+          .post(apiURL, this.game)
+          .then(() => {
+            this.$router.push("/view");
+            this.game = {
+              name: "",
+              email: "",
+              phone: "",
+            };
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      },
+    },
+  };
+  </script>
+  
---
+++ src/components/EditComponent.vue
@@ -0,0 +1,86 @@
+<template>
+    <div class="row justify-content-center">
+      <div class="col-md-6">
+        <h3 class="text-center">Update game</h3>
+        <form @submit.prevent="handleUpdateForm">
+          <div class="form-group">
+            <label>Name</label>
+            <input
+              type="text"
+              class="form-control"
+              v-model="game.name"
+              required
+            />
+          </div>
+  
+          <div class="form-group">
+            <label>Email</label>
+            <input
+              type="email"
+              class="form-control"
+              v-model="game.email"
+              required
+            />
+          </div>
+  
+          <div class="form-group">
+            <label>Phone</label>
+            <input
+              type="text"
+              class="form-control"
+              v-model="game.phone"
+              required
+            />
+          </div>
+          <div class="form-group">
+            <label>Images</label>
+            <input
+              type="img"
+              class="form-control"
+              v-model="game.image"
+              required
+            />
+          </div>
+  
+          <div class="form-group">
+            <button class="btn btn-danger btn-block">Update</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import axios from "axios";
+  
+  export default {
+    data() {
+      return {
+        game: {},
+      };
+    },
+    created() {
+      let apiURL = `http://localhost:4000/api/edit-game/${this.$route.params.id}`;
+  
+      axios.get(apiURL).then((res) => {
+        this.game = res.data;
+      });
+    },
+    methods: {
+      handleUpdateForm() {
+        let apiURL = `http://localhost:4000/api/update-game/${this.$route.params.id}`;
+  
+        axios
+          .put(apiURL, this.game)
+          .then((res) => {
+            console.log(res);
+            this.$router.push("/view");
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      },
+    },
+  };
+  </script>
+  
