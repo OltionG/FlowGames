8aca925b9c31ddd935132d1565bb7f74c8f04054
LorikPopaj <lp51861@ubt-uni.net>
2023-03-12 18:21:43 +0100

    components

--- src/router/index.js
+++ src/router/index.js
@@ -3,10 +3,26 @@ import { createRouter, createWebHistory } from "vue-router";
 const router = createRouter({
     history: createWebHistory(),
     routes: [
+        {
+    path: '/create',
+    name: 'create',
+    component: () => import('../components/CreateComponent'),
+  },
+  {
+    path: '/view',
+    name: 'view',
+    component: () => import('../components/ListComponent'),
+  },
+  {
+    path: '/edit/:id',
+    name: 'edit',
+    component: () => import('../components/EditComponent'),
+  },
         { path: "/", component: () => import("../views/Home.vue")},
         { path: "/register", component: () => import("../views/Register.vue")},
         { path: "/login", component: () => import("../views/Login.vue")},
-        { path: "/dashboard", component: () => import("../views/Dashboard.vue")}
+        { path: "/dashboard", component: () => import("../views/Dashboard.vue")},
+        { path: "/games", component: () => import("../views/Games.vue")}
     ]
 });
 
