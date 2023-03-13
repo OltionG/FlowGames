3d2a5c724f9cc0d517c67dbb472a8ea5c645f6ee
OltionG <gashioltion@gmail.com>
2023-03-13 13:10:55 +0100

    initial_home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,129 @@
+<script setup>
+
+</script>
+
+
 <template>
-    <h1>Home</h1>
-    <table>
-        <thead>
-            <tr>
-                <th>Name</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td> </td>
-            </tr>
-        </tbody>
-    </table>
+<main>
+      <!--Ketu fillon pjesa qe duhet te observohet nga Javascript per ndryshimin e Header-->
+      <div class="ob">
+      <section class="home-intro">
+        <img src="@/assets/logo.png" alt="Logo.png">
+        <h1>A continuous stream of entertainment</h1>
+      </section>
+      </div>
+      <!--Ketu fillon content-->
+      <div style="background-color:whitesmoke; height: 3%; opacity: 70%;">
+      </div>
+      <div class ="about" style=" letter-spacing: 1px; line-height: 2;">
+      <div class="home-about"><div class='abt-intro'>
+        <h2>Flow Entertainment</h2>
+        <p>Let the relaxing flow guide you.</p>
+        <br>
+        <hr style="background-color: black; border-width: 0; width: 100%; height: 1px;">
+      </div> 
+        <div class="columns">
+          <div style="border-right:1px solid gray;height:auto">
+            <h3>Who are we?</h3>
+            <p>
+              A small indie company, made by the dreams of some ambitious college students.
+            </p>
+          </div>
+          <div style="border-right:1px solid gray;height:auto">
+            <h3>What do we do?</h3>
+            <p>
+              Attemp to bring you video-games that provide you with the entertainment you rightfully deserve.
+            </p>
+          </div>
+          <div>
+            <h3>What's so special about you?</h3>
+            <p>
+              We try to work more with the community, and attempt to create something beautiful together.
+            </p>
+        </div>
+       </div>
+      </div>
+    </div>
+   <div class="about" style=" letter-spacing: 1px; line-height: 2;"><div class="abt-intro">
+     <h2>Small Company, Big Dream!</h2>
+     <br>
+    <p>We are Flow Entertainment, a small independent video-game developing company, trying to follow a dream. Our passion for creating
+      new things has brought us here: creating new games for you to enjoy. Although we are not too experienced yet, we've been making lots of progress
+      and soon enough we will have something to present to you, to show the fruits of our combined efforts. But what we need the most is not some
+      finished product. No. Instead, what we really need is you yourselves.
+    </p>
+    </div>
+    <div class="home-about"><div class='abt-intro' style="margin-top: -4%;">
+      <h2 style="color: lightblue; font-style: italic; font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif">All together now!</h2>
+      <br>
+      <hr style="background-color: black; border-width: 0; width: 100%; height: 1px;">
+    </div> 
+  <div class="img-columns">
+    <a href="about.html" style="text-decoration: none;"><div class="icon">
+      <img src="@/assets/joy.png" width="100px" height="100px">
+      <h3>Friendship!</h3>
+      <p>
+        Learn more about us so we can establish a trustful relationship!
+      </p>
+    </div>
+  </a>
+    <a href="community.html"  style="text-decoration: none;"><div class="icon">
+      <img src="@/assets/ideas.png" width="100px" height="100px">
+      <h3>Imagine!</h3>
+      <p>
+        The only boundary is your imagination! Think up ideas with the community!
+      </p>
+    </div>
+  </a>
+    <a href="contacts.html"  style="text-decoration: none;"><div class="icon">
+     <img src="@/assets/share.png" width="100px" height="100px">
+      <h3>Share!</h3>
+      <p>
+        Have something interesting cooked up? Contact us and tell us all about it!
+      </p>
+    </div>
+  </a>
+    <a href="login/create.html"  style="text-decoration: none;"><div class="icon">
+      <img src="@/assets/chat.png" width="100px" height="100px">
+      <h3>Chat!</h3>
+      <p>
+        Join our community and socialize! Talk with other fans around the world!
+      </p>
+    </div>
+  </a>
+  </div>
+   </div>
+   </div>
+      <div class="footer">
+        <div class="home-about-foot">
+          <div class="columns" style="flex-direction: column;">
+            <div style="margin: 2%;display: flex; flex-direction: row; justify-content: space-evenly;">
+              <a href="main.html" class="logo" aria-label="homepage">flow.</a>            
+              <ul class="nav_list" style="margin-top: -5px;">
+                <li class="nav_list-item"><a href="main.html" class="nav_link">Home</a></li>
+                <li class="nav_list-item"><a href="about.html" class="nav_link">About</a></li>
+                <li class="nav_list-item"><a href="gallery.html" class="nav_link">Gallery</a></li>
+                <li class="nav_list-item"><a href="community.html" class="nav_link">Community</a></li>
+                <li class="nav_list-item"><a href="contacts.html" class="nav_link">Contacts</a></li>
+              </ul>
+                <a target="_blank" href="https://www.facebook.com/Flow-Entertainment-100628881531882/?modal=admin_todo_tour%22%3E"><img class="ftimg" src="@/assets/facebook.png" height="30" width="30" alt="Facebook.png"></a>
+                <a target="_blank" href="https://www.instagram.com/flow.entertainment.games/%22%3E"><img class="ftimg" src="@/assets/instagram.png" height="30" width="30" alt="Instagram.png"></a>
+                <a target="_blank" href="https://www.youtube.com/channel/UCk1H9Uwr8k82EouBomBjosA?view_as=subscriber%22%3E"><img src="@/assets/youtube.png" height="30" width="30" alt="Youtube.png"></a>
+            </div>
+            <hr>
+            <div style="margin: 2%;display: flex; flex-direction: row; justify-content: space-evenly; align-items: center;">
+            <div style="font-size: 11px;">
+              <h2>Our Company</h2>
+              <p>Flow Entertainment aims to create entertainment for you, from you, and with you.</p>
+            </div>
+            <hr style="height: 50px; margin: 2%;">
+            <div style="font-size: 10px; opacity: 80%;">© 2021 Flow Entertainment, Inc, All Rights Reserved.</div>
+          </div>
+           </div>
+          </div>
+        </div>
+    </main>
 </template>
 
-<script setup>
-</script>
+
+
